7802b4f1b8748636691963324060328fe3c87088
John Doe <omodaraodubiro@gmail.com>
2022-09-09 11:38:26 -0700

    software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
-
 /**
- *  * main - Printing combinations of two two-digit numbers.
- *   *
- *    * Return: Always 0.
- *     */
+ *
+ * * main - Printing combinations of two two-digit numbers.*
+ *
+ * * Return: Always 0. */
+
 int main(void)
+
 {
-		int nb1, nb2;
+	int nb1, nb2;
 
-			for (nb1 = 0; nb1 <= 98; nb1++)
-					{
-								for (nb2 = nb1 + 1; nb2 <= 99; nb2++)
-											{
-															putchar((nb1 / 10) + '0');
-																		putchar((nb1 % 10) + '0');
-																					putchar(' ');
-																								putchar((nb2 / 10) + '0');
-																											putchar((nb2 % 10) + '0');
+	for (nb1 = 0; nb1 <= 98; nb1++)
 
-																														if (nb1 == 98 && nb2 == 99)
-																																			break;
+	{
 
-																																	putchar(',');
-																																				putchar(' ');
-																																						}
-									}
+		for (nb2 = nb1 + 1; nb2 <= 99; nb2++)
 
-				putchar('\n');
+		{
+			putchar((nb1 / 10) + '0');
+			putchar((nb1 % 10) + '0');
+			putchar(' ');
+			putchar((nb2 / 10) + '0');
+			putchar((nb2 % 10) + '0');
 
-					return (0);
+			if (nb1 == 98 && nb2 == 99)
+
+				break;
+
+			putchar(',');
+			putchar(' ');
+
+					}
+
+			}
+
+
+
+		putchar('\n');
+
+
+
+			return (0);
+
 }
