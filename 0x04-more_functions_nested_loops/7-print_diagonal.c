a939cc874c8d6e6fe2a56f693f1b79d8ae76b8be
omodaraodubiro <omodaraodubiro@gmail.com>
2022-09-15 06:08:11 -0700

    new

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,34 @@
-#include"main.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * print_diagonal - print a diagonal line
- *
- * @n: is the number of times the \ character
- *     should be printed
+*print_diagonal - prints a diagonal
+*@n: parameter
+*Return: returns nothing
 */
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int len, space;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	if (n > 0)
 	{
-		for (postn = 1; postn <= n; ++postn)
+		for (len = 0; len < n; len++)
 		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
+			for (space = 0; space < len; space++)
+			{
+				putchar(' ');
+			}
+
+			putchar('\\');
+
+			if (len == (n - 1))
+			{
+				continue;
+			}
+			putchar('\n');
 		}
 	}
+	putchar('\n');
 }
+
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,32 @@
-#include"main.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * print_square - print a square using the character #
- *
- * @size: is the size of the square
- *
- * Return: Always 0 (Success)
+*print_square - prints squares
+*@size: parameter
+*Return: returns nothing
 */
 
 void print_square(int size)
 {
-	int row, column;
+	int inc1, inc2;
 
-	for (row = 1; row <= size; ++row)
+	if (size > 0)
 	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
-		_putchar('\n');
+		for (inc1 = 0; inc1 < size; inc1++)
+		{
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			{
+				putchar('#');
+			}
+
+			putchar('#');
+			putchar('\n');
+		}
 	}
+	else
+	{
+		putchar('\n');
+	}
+}
 }
